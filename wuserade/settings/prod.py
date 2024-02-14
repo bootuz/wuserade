c424034bd36869dfc7e370c8e31ccbc4c56a084b
Astemir Boziev <bootuz07@gmail.com>
2024-02-14 22:38:37 +0400

    deploy preparation

--- wuserade/settings/prod.py
+++ wuserade/settings/prod.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from dj_database_url import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
