4bd1ff9f6a9566be9253f6e156d0642c3a83ae75
Astemir Boziev <bootuz07@gmail.com>
2025-04-20 19:32:25 +0400

    added new endpoints for web site

--- poems/api_views.py
+++ poems/api_views.py
@@ -16,7 +16,8 @@ from .serializers import (
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 20
-    page_size_query_param = 'page'
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
     max_page_size = 100
 
 
