be6ca9d935db37a4f70e8361bcdd66b83fa71525
Astemir Boziev <bootuz07@gmail.com>
2025-05-02 13:17:27 +0400

    Set ALLOWED_HOSTS for production environment
    Updated ALLOWED_HOSTS to include 'wuserade.com' and 'www.wuserade.com'. This ensures proper handling of requests in the production environment and improves deployment readiness.

--- wuserade/settings/prod.py
+++ wuserade/settings/prod.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "you_never_guess")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wuserade.com', 'www.wuserade.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
